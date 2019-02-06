b2de8b9f25a16684d33ac80c1f06366db2c2c3d0
diehmer86 <maxxdiehm26@gmail.com>
2019-02-06 12:15:45 -0800

    2/6/18

--- Spongebob adventures/behavior-hx/SceneEvents_1.hx
+++ Spongebob adventures/behavior-hx/SceneEvents_1.hx
@@ -40,6 +40,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -69,18 +70,61 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_17 extends ActorScript
+class SceneEvents_1 extends SceneScript
 {
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
+		super();
 		
 	}
 	
 	override public function init()
 	{
+		
+		/* ======================== When Creating ========================= */
+		createRecycledActor(getActorType(4), 9, 302, Script.FRONT);
+		createRecycledActor(getActorType(6), 638, 226, Script.FRONT);
+		createRecycledActor(getActorType(13), 500, 226, Script.FRONT);
+		
+		/* ======================== When Creating ========================= */
+		createRecycledActor(getActorType(4), 9, 302, Script.FRONT);
+		createRecycledActor(getActorType(6), 638, 226, Script.FRONT);
+		createRecycledActor(getActorType(13), 500, 226, Script.FRONT);
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				if((getLastCreatedActor().getScreenX() < 0))
+				{
+					getLastCreatedActor().setX(1);
+				}
+				else if((getLastCreatedActor().getScreenX() > (getScreenWidth() - (getLastCreatedActor().getWidth()))))
+				{
+					getLastCreatedActor().setX(((getScreenWidth() - (getLastCreatedActor().getWidth())) - 1));
+				}
+				if((getLastCreatedActor().getScreenY() < 0))
+				{
+					getLastCreatedActor().setY(1);
+				}
+				else if((getLastCreatedActor().getScreenY() > (getScreenWidth() - (getLastCreatedActor().getWidth()))))
+				{
+					getLastCreatedActor().setY(((getScreenWidth() - (getLastCreatedActor().getWidth())) - 1));
+				}
+			}
+		});
+		
+		/* ======================== Specific Actor ======================== */
+		addActorEntersRegionListener(getRegion(3), function(a:Actor, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && sameAs(getActor(1), a))
+			{
+				switchScene(GameModel.get().scenes.get(2).getID(), null, createSlideDownTransition(1.01));
+			}
+		});
 		
 	}
 	
